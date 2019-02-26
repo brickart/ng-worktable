5b177d596bb1d878d28340844d8e4bea365c8acb
Artem Rebryk <arebr@softserveinc.com>
2019-02-26 17:57:10 +0200

    create base construction of worktable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentModule } from './component/component.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
+    ComponentModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/component/component.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+
+@NgModule({
+  declarations: [CheckboxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentModule { }
